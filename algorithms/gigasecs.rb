b8468852f5095a6176797e7d6ca0c80d25ba54c8
Dave Spina <dspina79@gmail.com>
2021-08-22 21:14:46 -0400

    Added gigasecs determination

---
+++ algorithms/gigasecs.rb
@@ -0,0 +1,25 @@
+SECOND_HOUR = 3600
+SECOND_DAY = SECOND_HOUR * 24
+SECOND_YEAR = SECOND_DAY * 365.25
+
+megasecond = 1000000
+gigasecond = megasecond * 1000
+terasecond = gigasecond * 1000
+petasecond = terasecond * 1000
+
+puts "1 megasecond = "
+puts megasecond / SECOND_YEAR
+puts "years"
+
+puts "1 gigasecond = "
+puts gigasecond / SECOND_YEAR
+puts "years"
+
+puts "1 terasecond = "
+puts terasecond / SECOND_YEAR
+puts "years"
+
+puts "1 petasecond = "
+puts petasecond / SECOND_YEAR
+puts "years"
+
