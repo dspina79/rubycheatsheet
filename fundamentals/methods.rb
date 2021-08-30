b37baa30901070b6751545dab584eeb198ac55b8
Dave Spina <dspina79@gmail.com>
2021-08-29 20:13:21 -0400

    Methods with no explict return

--- fundamentals/methods.rb
+++ fundamentals/methods.rb
@@ -12,14 +12,21 @@ def square(x)
     return x * x
 end
 
-result = add(3,4)
-puts result # outputs 7
+result_square = square(15)
+puts result_square # outputs 225 
 
 # method with two parameters
 def add(x, y)
     return x + y
 end
 
-result_square = square(15)
-puts result_square # outputs 225 
+result = add(3,4)
+puts result # outputs 7
 
+# methods with implied return parameters
+def square2(num)
+    num ** 2 # the same as return num ** 2
+end
+
+puts "The square of 16 is #{square2(16)}"
+# ^^^ outputs The square of 16 is 256
