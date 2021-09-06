f8b9cc811e48aa6444a2c6095007dc41a90b390d
Dave Spina <dspina79@gmail.com>
2021-09-06 10:38:04 -0400

    Started modules

---
+++ modules/modile1/implementation.rb
@@ -0,0 +1,4 @@
+require_relative "maths.rb"
+
+puts Maths.add(4, 3) # outputs 7
+puts Maths.square(16) # outputs 256
---
+++ modules/modile1/maths.rb
@@ -0,0 +1,21 @@
+module Maths
+    def Maths.add(x, y)
+        return x + y
+    end
+
+    def Maths.subtract(x, y)
+        return x - y
+    end
+
+    def Maths.multiply(x, y)
+        return x * y
+    end
+    
+    def Maths.divide(x, y)
+        return x / y
+    end
+
+    def Maths.square(x)
+        return x * x
+    end
+end
